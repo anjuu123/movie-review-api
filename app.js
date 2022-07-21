4367cabfaec2824d6a11c07d3e479194c6cc98fe
Anju Dhungana <anjudhungana28@gmail.com>
2022-07-21 17:06:17 -0400

    database connection done

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+require('./db');
 const userRouter = require('./routes/user')
 
+
 const app = express();
-app.use('/api', userRouter);
+app.use(express.json());
+app.use('/api/user', userRouter);
 
 
 
