77e2aac4c9d65d3c80a15187c0b041295c509541
Anju Dhungana <anjudhungana28@gmail.com>
2022-07-20 20:00:27 -0400

    backend server setup is done

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log(10+5);
+const express = require('express');
+
+const app = express();
+app.get('/', (req,res) => {
+    res.send('<h1>Hello I am From your backend</h1>')
+})
+
+
+app.listen(8000, () => {
+    console.log("the port is running")
+})
