1d7efd683726b0639cde99b83e3377bf13479c71
Anju Dhungana <anjudhungana28@gmail.com>
2022-07-27 23:35:02 -0400

    import middleware for user validation

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { check } = require('express-validator');
 const { create } = require("../controllers/user");
+const { userValidtor, validate } = require('../middlewares/validator');
 
 const router = express.Router();
 
-router.post("/create", create);
+router.post("/create", userValidtor , validate,create);
 
 
 module.exports = router;
