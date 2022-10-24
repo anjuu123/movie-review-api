461e2c9d9fb2f3363936e77c31e9633489227b38
Anju Dhungana <anjudhungana28@gmail.com>
2022-10-24 11:28:17 -0400

    middleware to check reset password

---
+++ middlewares/user.js
@@ -0,0 +1,18 @@
+const { isValidObjectId } = require("mongoose");
+const { sendError } = require("next/dist/server/api-utils");
+const PasswordResetToken = require("../models/passwordResetToken");
+exports.isValidPassResetToken = async (req, res, next) => {
+  const { token, userId } = req.body;
+
+  if (!token.trim() || isValidObjectId(userId))
+    return sendError(res, "Invalid request!");
+
+  const resetToken = await PasswordResetToken.findOne({ owner: userId });
+  if (!resetToken) return sendError(res, "Unauthorized access, invalid request!");
+
+  const matched = await resetToken.compareToken(token);
+  if (!matched) return sendError(res, "Unauthorized access, invalid request!");
+
+  req.resetToken = resetToken;
+  next();
+};
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const { check } = require("express-validator");
-const {
-  create,
-  verifyEmail,
-  resendEmailVerificationToken,
-  forgetPassword,
-} = require("../controllers/user");
+const {create,verifyEmail,resendEmailVerificationToken,forgetPassword} = require("../controllers/user");
 const { userValidtor, validate } = require("../middlewares/validator");
+const { isValidPassResetToken } = require("../middlewares/user");
 
 const router = express.Router();
 
@@ -14,5 +10,8 @@ router.post("/create", userValidtor, validate, create);
 router.post("/verify-email", verifyEmail);
 router.post("/resend-token", resendEmailVerificationToken);
 router.post("/forget-password", forgetPassword);
+router.post("/verify-pass-reset-token", isValidPassResetToken,(req, res) => {
+ res.json({ valid: true })
+});
 
 module.exports = router;
